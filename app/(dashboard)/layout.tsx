f3a1af6711289c2c403f36bd7fd2a43c5fdedf90
NgothTsika <ngothtsikavivien@gmail.com>
2025-03-06 17:21:26 +0000

    sidebar added

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,11 @@
+import { Sidebar } from "./_components/sidebar";
+
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className=" h-full md:flex hidden w-56 flex-col fixed inset-y-0 z-50">
+    <div className=" h-full">
+      <div className=" md:flex hidden w-56 flex-col fixed inset-y-0 z-50">
+        <Sidebar />
+      </div>
       {children}
     </div>
   );
