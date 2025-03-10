b99033e6c068fe686117d3451f89a945e696f633
NgothTsika <ngothtsikavivien@gmail.com>
2025-03-10 23:59:47 +0000

    edite

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,7 +1,7 @@
 import { createRouteHandler } from "uploadthing/next";
 import { ourFileRouter } from "./core";
 
-console.log("✅ /api/uploadthing route is active!");
+console.log("✅ /api/uploadthing route is active !");
 
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
